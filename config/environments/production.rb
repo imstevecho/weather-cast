b0b4af0f0b2cbd4a8d16fa6e52b099ce996535d1
Steve Cho <iamstevecho@gmail.com>
2024-02-02 10:26:16 -0500

    Code cleanup
    code cleanup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "zipcast_production"
+  # config.active_job.queue_name_prefix = "weather_cast_production"
 
   config.action_mailer.perform_caching = false
 
