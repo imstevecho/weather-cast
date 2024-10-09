412e55bc05d1b240f6cfae5cb6bb569bfd534320
Steve Cho <iamstevecho@gmail.com>
2024-10-08 22:51:40 -0400

    retry worked

--- app/controllers/weathers_controller.rb
+++ app/controllers/weathers_controller.rb
@@ -1,8 +1,5 @@
 class WeathersController < ApplicationController
   def index
-    @error = nil
-    @forecast = nil
-
     if params[:q].present?
       geocode_service = GeocodeService.new
       begin
@@ -10,17 +7,30 @@ class WeathersController < ApplicationController
 
         if location_info[:data]
           forecast_service = ForecastService.new(location_info[:data][:country_code])
-          @forecast = forecast_service.with_lat_lon(location_info[:data][:lat], location_info[:data][:lon])
+          @forecast_data = forecast_service.with_lat_lon(location_info[:data][:lat], location_info[:data][:lon])
+          @zip = location_info[:data][:zip]
+          @is_from_cache = location_info[:is_from_cache]
         else
-          @error = "Unable to find location information"
+          flash[:error] = "Unable to find location information"
         end
       rescue GeocodeService::GeocodingError => e
-        @error = "Geocoding error: #{e.message}"
+        flash[:error] = "Geocoding error: #{e.message}"
       rescue ForecastService::ForecastError => e
-        @error = "Forecast error: #{e.message}"
+        flash[:error] = "Forecast error: #{e.message}"
       rescue StandardError => e
-        @error = "An unexpected error occurred: #{e.message}"
+        flash[:error] = "An unexpected error occurred: #{e.message}"
       end
     end
   end
+
+  private
+
+  def human_readable_time(time)
+    time.strftime("%A, %B %d, %Y %I:%M %p")
+  end
+
+  def is_today?(date)
+    date.to_date == Date.today
+  end
+  helper_method :human_readable_time, :is_today?
 end
--- app/services/geocode_service.rb
+++ app/services/geocode_service.rb
@@ -18,15 +18,19 @@ class GeocodeService
 
   def coords_by_zipcode(zipcode, country_code = 'US', skip_cache: false)
     query = "#{zipcode},#{country_code}"
-    fetch_geo_info('zipcode', zipcode, query, skip_cache: skip_cache)
+    Rails.logger.info "GeocodeService: Fetching coords for #{query}"
+    result = fetch_geo_info('zipcode', zipcode, query, skip_cache: skip_cache)
+    Rails.logger.info "GeocodeService: Result for #{query}: #{result.inspect}"
+    result
   end
 
   private
 
   def fetch_geo_info(prefix, key, query, skip_cache: false)
     cache_key = cache_key(prefix, key)
+    Rails.logger.info "GeocodeService: Cache key: #{cache_key}"
     CachingService.fetch(cache_key, expires_in: CACHE_EXPIRATION, skip_cache: skip_cache) do
-      Rails.logger.info "Fetching geocode for #{query}"
+      Rails.logger.info "GeocodeService: Cache miss, fetching from API for #{query}"
       with_retries(max_retries: MAX_RETRIES) { geocode(query) }
     end
   rescue StandardError => e
@@ -34,8 +38,11 @@ class GeocodeService
   end
 
   def geocode(query)
+    Rails.logger.info "GeocodeService: Geocoding #{query}"
     parsed_response = fetch_from_api(query)
-    extract_location_info(parsed_response)
+    result = extract_location_info(parsed_response)
+    Rails.logger.info "GeocodeService: Geocoding result for #{query}: #{result.inspect}"
+    result
   end
 
   def fetch_from_api(address)
